8fe22b0dcc231e008afe73dc5bf9410bb038b632
khans25 <khans25@vcu.edu>
2016-09-22 13:44:32 -0400

    Added user entries in

--- phonebook/phonebook.cpp
+++ phonebook/phonebook.cpp
@@ -9,8 +9,26 @@ using namespace std;
 const int MIN = 10;
 const int MAX = 20;
 
+void showlist(char mylist[][MAX], int numEntered);
+void AddName(char mylist[][MAX]);
+
 int main()
 {
+	char nameList[MIN][MAX];
+	int i;
+	char answer = 'y';
+
+	for (i = 0; i < MIN && (answer == 'y' || answer == 'Y'); i++)
+	{
+		AddName(nameList);
+		cout << "Do you have additional names to enter? Y = Yes, N = No. " << endl;
+		cin >> answer;
+		cin.clear();
+		cin.ignore();
+	}
+	cout << "Names entered: " << i << endl;
+	showlist(nameList, i);
+
     return 0;
 }
 
