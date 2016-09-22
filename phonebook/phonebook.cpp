b816c26d06d4128983695d335a4511c3b70019db
khans25 <khans25@vcu.edu>
2016-09-22 13:38:41 -0400

    Added MIN/MAX

---
+++ phonebook/phonebook.cpp
@@ -0,0 +1,16 @@
+// phonebook.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+const int MIN = 10;
+const int MAX = 20;
+
+int main()
+{
+    return 0;
+}
+
